4be2bb087f1988873d77f00e8ad4d212497cf44a
saerielle <129182983+saerielle@users.noreply.github.com>
2025-07-18 10:56:54 +0300

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using T17.Flow;
+
+namespace NoSplashScreen;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        IdentManager.EnableForceSkip();
+    } 
+}
